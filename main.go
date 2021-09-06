1c2b0712982fee34a7135a541d2e13e2ae992ec5
Levi Durfee <levi.durfee@gmail.com>
2021-09-06 13:47:40 -0400

    Format output

--- main.go
+++ main.go
@@ -13,8 +13,12 @@ func main() {
 	me.Wireguard()
 	me.Bird()
 
+	fmt.Println("#########################")
+	fmt.Println()
 	fmt.Printf("wg-quick up %s\n", me.Peer.Name)
 	fmt.Printf("systemctl enable wg-quick@%s\n", me.Peer.Name)
 	fmt.Println("birdc config check")
 	fmt.Println("birdc config")
+	fmt.Println()
+	fmt.Println("#########################")
 }
